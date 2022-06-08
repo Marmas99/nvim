b5b6c55522eb93df6c8376916d0d4e674354c68f
Markus <markus.masalkovski@gmail.com>
2022-06-08 10:31:34 +0200

    added package

--- lua/packages.lua
+++ lua/packages.lua
@@ -1,4 +1,10 @@
 require('packer').startup(function()
   use 'wbthomason/packer.nvim'
+  use {
+    'goolord/alpha-nvim',
+    config = function ()
+        require'alpha'.setup(require'alpha.themes.dashboard'.config)
+    end
+  }
 end)
 
