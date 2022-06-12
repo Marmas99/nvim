bbdf214d996656fd24b20fcf957eb61df688edb4
Markus <markus.masalkovski@gmail.com>
2022-06-12 19:30:46 +0200

    configured alpha greeter with buttons and added telescope plugin

--- lua/packages.lua
+++ lua/packages.lua
@@ -2,10 +2,17 @@ require('packer').startup(function()
   use 'wbthomason/packer.nvim'
   use {
     'goolord/alpha-nvim',
+    requires = { 'kyazdani42/nvim-web-devicons' },
     config = function ()
-        require'alpha'.setup(require'alpha.themes.dashboard'.config)
+        require'alpha'.setup(require'alpha.themes.startify'.config)
+        local startify = require("alpha.themes.startify")
+        startify.section.bottom_buttons.val = {
+          startify.button("e", "new file", ":ene <bar> startinsert <cr>"),
+          startify.button("f", "find file", ":Telescope find_files <cr>"),
+	  startify.button("q", "quit neovim", ":qa <cr>")
+        }
     end
-  }
+}
   use {
   'nvim-telescope/telescope.nvim',
   requires = { {'nvim-lua/plenary.nvim'} }
