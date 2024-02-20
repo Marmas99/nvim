2a8f25b14e368841ddc55e4500438d7a69af4bb6
Markus Masalkovski <markus.masalkovski@axis.com>
2024-02-20 15:24:53 +0100

    added header to greeter

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -3,7 +3,17 @@ return {
   event = 'VimEnter',
   config = function()
     require('dashboard').setup {
-      -- config
+      shortcut_type = "number", --  shorcut type 'letter' or 'number'
+      config = {
+         header = {
+           ' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
+           ' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
+           ' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
+           ' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
+           ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
+           ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
+         }  -- type is table def
+      }
     }
   end,
   dependencies = { {'nvim-tree/nvim-web-devicons'}}
