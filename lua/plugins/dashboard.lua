072e175a2bacfcb26e4a810cddbf2edf7a2d9280
Markus Masalkovski <markus.masalkovski@axis.com>
2024-02-21 08:29:13 +0100

    started using doom theme

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -6,14 +6,19 @@ return {
       shortcut_type = "number", --  shorcut type 'letter' or 'number'
       theme = "doom",
       config = {
-         header = {
-           ' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
-           ' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
-           ' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
-           ' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
-           ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
-           ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
-         }  -- type is table def
+        center = {
+          {
+            icon = '',
+            icon_hl = 'group',
+            desc = 'description',
+            desc_hl = 'group',
+            key = 'shortcut key in dashboard buffer not keymap !!',
+            key_hl = 'group',
+            key_format = ' [%s]', -- `%s` will be substituted with value of `key`
+            action = '',
+          },
+        },
+        footer = {},
       }
     }
   end,
