26c72c79f3d12db234be72b9ecd087449f9daf7a
Markus Masalkovski <markus.masalkovski@axis.com>
2024-02-26 09:04:59 +0100

    added some keymaps

--- lua/plugins/troube-nvim.lua
+++ lua/plugins/troube-nvim.lua
@@ -1,9 +1,15 @@
 return {
  "folke/trouble.nvim",
  dependencies = { "nvim-tree/nvim-web-devicons" },
- opts = {
-  -- your configuration comes here
-  -- or leave it empty to use the default settings
-  -- refer to the configuration section below
- },
+ config =function ()
+  local opts = {
+
+  }
+
+  local keymap = vim.keymap
+
+
+  keymap.set("n", "fe", "<cmd>Trouble workspace_diagnostics<cr>", { desc = "Find errors" })
+  keymap.set("n", "tq", "<cmd>TroubleClose<cr>", { desc = "Quit trouble" })
+ end,
 }
