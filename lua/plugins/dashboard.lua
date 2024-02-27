f30118454693716784e68655236f61a1d7b0edf4
Markus Masalkovski <markus.masalkovski@axis.com>
2024-02-27 09:19:45 +0100

    added another key

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -27,12 +27,22 @@ return {
           {
             icon = '☇     ',
             icon_hl = 'Title',
-            desc = 'Find Files     ',
+            desc = 'Find Files',
             desc_hl = 'group',
             key = 'ff',
             key_hl = 'group',
             key_format = ' [%s]', -- `%s` will be substituted with value of `key`
             action = ':Telescope find_files',
+          },
+          {
+            icon = '☄     ',
+            icon_hl = 'Title',
+            desc = 'Update Plugins',
+            desc_hl = 'group',
+            key = 'u',
+            key_hl = 'group',
+            key_format = ' [%s]', -- `%s` will be substituted with value of `key`
+            action = ':Lazy update',
           },
         },
         footer = {
