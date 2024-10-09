225afae5b4705f6921e5e0f213fb6ac2f4ae673b
Markus Masalkovski <markus.masalkovski@axis.com>
2024-10-09 11:17:58 +0200

    added lspsaga keymappings to lspsaga plugin file

--- lua/plugins/lsp-saga.lua
+++ lua/plugins/lsp-saga.lua
@@ -1,6 +1,16 @@
 return {
     'nvimdev/lspsaga.nvim',
     config = function()
+      local keymap = vim.api.nvim_set_keymap
+      local default_opts = { noremap = true, silent = true }
+
+      -- Hover keyword
+      keymap("n", "K", ":Lspsaga hover_doc<CR>", default_opts)
+
+      -- Rename variable
+      keymap("n", "rn", ":Lspsaga rename<CR>", default_opts)
+
+
         require('lspsaga').setup({})
     end,
     dependencies = {
