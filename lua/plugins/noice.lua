a31dfba82c2fb90eec229e412d0212d4dbcad032
Markus Masalkovski <markus.masalkovski@axis.com>
2024-10-09 16:17:46 +0200

    got it positioned

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -5,34 +5,40 @@ return {
 		"MunifTanjim/nui.nvim",
     "rcarriga/nvim-notify",
 	},
-	opts = {
-      signature = {
-        enabled = false,  -- Disable LSP signature help
+
+  config = function ()
+    require("noice").setup({
+      views = {
+        cmdline_popup = {
+          position = {
+            row = 20,
+            col = "50%",
+          },
+        },
+        popupmenu = {
+          relative = "editor",
+          position = {
+              row = 23,
+              col = "50%",
+          },
+          border = {
+            style = "rounded",
+            padding = { 0, 1 },
+          },
+          size = {
+            width = 60,
+            height = 10,
+          },
+          win_options = {
+            winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+          },
+        },
       },
-		routes = {
-			{
-				view = "notify",
-				filter = { event = "msg_showmode" },
-			},
-		},
-		lsp = {
-			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-      signature = {
-        enabled = false,  -- Disable LSP signature help
-      },
-			override = {
-				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-				["vim.lsp.util.stylize_markdown"] = true,
-				["cmp.entry.get_documentation"] = true,
-			},
-		},
-		-- you can enable a preset for easier configuration
-		presets = {
-			bottom_search = true,
-			command_palette = true,
-			long_message_to_split = true,
-			inc_rename = false,
-			lsp_doc_border = true,
-		},
-	},
+      lsp = {
+        signature = {
+          enabled = false,
+        }
+      }
+    })
+  end,
 }
