f2c539f1bf690d6ff0aae67ba4b4f3ec1013f263
Markus <markusma@axis.com>
2026-09-07 16:24:23 +0200

    fixed treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,24 +1,21 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
+	build = ":TSUpdate",
 	config = function()
-		require'nvim-treesitter.configs'.setup {
-			-- A list of parser names, or "all" (the five listed parsers should always be installed)
-			ensure_installed = { "go", "lua", "json",},
+		local ensure_installed = { "go", "lua", "json" }
 
-			-- Install parsers synchronously (only applied to `ensure_installed`)
-			sync_install = false,
+		require("nvim-treesitter").setup()
+		require("nvim-treesitter").install(ensure_installed)
 
-			-- Automatically install missing parsers when entering buffer
-			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-			auto_install = true,
-
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = true,
-			},
-			autopairs = {
-				enable = true,
-			},
-		}
+		-- Enable highlighting (and indenting) for the ensured parsers,
+		-- replacing the old `highlight.enable` / `indent.enable` options.
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = ensure_installed,
+			callback = function()
+				vim.treesitter.start()
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
+		})
 	end,
 }
